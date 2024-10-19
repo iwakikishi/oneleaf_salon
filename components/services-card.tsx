c72e40ec2e90eed810259ce7e075d1b376d91a24
oneleaf <iwaki.kishi@gmail.com>
2024-10-19 14:14:19 +0900

    10/20-14:14

--- components/services-card.tsx
+++ components/services-card.tsx
@@ -17,7 +17,7 @@ export function ServicesCardComponent() {
   return (
     <>
       {services.map((service) => (
-        <div className='flex flex-col aspect-[4/3] mt-2 bg-[#fffaeb] p-8 gap-6'>
+        <div key={service.title} className='flex flex-col aspect-[4/3] mt-2 bg-[#fffaeb] p-8 gap-6'>
           <Image src={`/images/${service.image}.webp`} alt={service.title} className='object-cover rounded-lg' width={500} height={120} />
           <p className='text-green-900 text-xl font-bold font-poppins'>{service.title}</p>
           <p className='text-gray-500 text-md break-words font-poppins'>{service.description}</p>
